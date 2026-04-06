fb723aee4a3778d3f186d6a0c4fef12f0d9f2a46
kundan100 <kundan100@yahoo.com>
2026-04-06 18:21:05 +0530

    update approach to open the file

--- src/args/appConfig/appConfig.js
+++ src/args/appConfig/appConfig.js
@@ -13,8 +13,10 @@ function printAppConfigDetails() {
     console.log("Update 'userConfig' manually in config.js to match your environment (e.g. repo paths), then use this command to verify the details.");
 }
 function openAppConfigFile() {
-    const open = require('open');
-    open(filePathForAppConfig);
+    // const open = require('open');
+    // open(filePathForAppConfig);
+    const { exec } = require("child_process");
+    exec(`start "" "${filePathForAppConfig}"`);
 }
 
 function appConfig() {
