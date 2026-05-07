f1ffcfa80ee7dad3e171b4e08350cc206054f8a9
kundan100 <kundan100@yahoo.com>
2026-05-07 17:46:15 +0530

    added a new config manager

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 // this is to show color outputs in terminal
 process.env.FORCE_COLOR = process.env.FORCE_COLOR || '1';
 
-// const { loadLocalAppConfigShadow } = require('./appConfig/appConfig');
-const { loadLocalAppConfigShadow } = require('#args/appConfig/appConfig');
+// const { loadLocalAppConfigShadow } = require('#args/appConfig/appConfig');
+const { createShadowOfAllConfigFiles } = require('#features/configManager/configManager');
 const handleArgs = require('#args/handleArgs');
 const pkg = require('./package.json');
 const clog = require('#shared/clog-with-fallback');
@@ -15,7 +15,8 @@ clog.log(`Welcome pater (v${pkg.version})!`);
 
 // create a local shadow of the app config in the user's home directory (e.g. c:/users/<user-name>/__cyk/.pater/config.js)
 // this is where tool's config is shadowed as backup and will be loaded on each run of the tool, so that any changes to the config file are reflected without needing to restart the CLI
-loadLocalAppConfigShadow();
+// loadLocalAppConfigShadow();
+createShadowOfAllConfigFiles();
 
 // capture CLI arguments (excluding `node` and script path)
 const args = process.argv.slice(2);
---
+++ src/features/configManager/configManager.js
@@ -0,0 +1,46 @@
+const configManagerJson = require('./configManager.json');
+const path = require('path');
+const fs = require('fs');
+const os = require('os');
+const clog = require('#shared/clog-with-fallback');
+
+// helpers
+const _helpers = {
+    // path for user-machine e.g. c:/users/<user-name>/
+    userHomePath: os.homedir(),
+    projectRoot: path.dirname(require.resolve('#root/package.json')),
+    localShadowOfProjectRoot: path.join(os.homedir(), '__cyk', ...require('#root/package.json').name.split('/'))
+};
+
+function createShadowOfAllConfigFiles() {
+    // The actual config manager code will be implemented in this file later.
+    clog.debug("createShadowOfAllConfigFiles...");
+    clog.debug(" - userHomePath:", _helpers.userHomePath);
+    clog.debug(" - path for project root (not the current directory):", _helpers.projectRoot);
+    clog.debug(" - local shadow of project root:", _helpers.localShadowOfProjectRoot);
+    clog.debug("configManagerJson.length: ", configManagerJson.length);
+    for (const configItem of configManagerJson) {
+        clog.debug(` - Config File Path (in json): ${configItem.filePath}, Config ID: ${configItem.id}`);
+        // source file
+        const sourceFilePath = path.resolve(path.join(_helpers.projectRoot, configItem.filePath));
+        clog.debug(` - sourceFilePath: ${sourceFilePath}`);
+        // ensure source file exists
+        if (!fs.existsSync(sourceFilePath)) {
+            clog.warn(`Source file not found for config ID "${configItem.id}" at path: ${sourceFilePath}. Skipping this config item.`);
+            continue; // skip to the next config item
+        }
+        // destination file
+        const destinationFilePath = path.join(_helpers.localShadowOfProjectRoot, configItem.filePath);
+        // Ensure destination folders exist
+        fs.mkdirSync(
+            path.dirname(destinationFilePath),
+            { recursive: true }
+        );
+        clog.debug(` - destinationFilePath: ${destinationFilePath} --- exists or created (if not existed)`);
+        // Copy file
+        fs.copyFileSync(sourceFilePath, destinationFilePath);
+        clog.log(`Copied config file for config ID "${configItem.id}" to local shadow: ${destinationFilePath}`);
+    }
+}
+
+module.exports = { createShadowOfAllConfigFiles };
